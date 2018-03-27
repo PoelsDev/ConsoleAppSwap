a47c3dcf3a63cf26aa70922092dff7dcc1c3d3fc
ThibaultP-immalle <31651240+ThibaultP-immalle@users.noreply.github.com>
2018-03-27 13:38:19 +0200

    Program

--- ConsoleAppSwap/Program.cs
+++ ConsoleAppSwap/Program.cs
@@ -9,6 +9,21 @@ namespace ConsoleAppSwap
     {
         static void Main(string[] args)
         {
+            int a = 4;
+            int b = 6;
+            Console.WriteLine(a);
+            Console.WriteLine(b);
+            Swap(ref a, ref b);
+            Console.WriteLine(a);
+            Console.WriteLine(b);
+        }
+
+        static void Swap(ref int a, ref int b)
+        {
+            int aCopy;
+            aCopy = a;
+            a = b;
+            b = aCopy;
         }
     }
 }
